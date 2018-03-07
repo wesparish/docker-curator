776b4c02868f3390dfe62046c086a4a87a27d174
wes <wes@home.local>
2018-03-06 18:37:12 -0600

    Adding tempsensor- index

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t wesparish/curator:5.4.1 . && \
-  docker push wesparish/curator:5.4.1
+docker build -t wesparish/curator:5.4.1-1.1 . && \
+  docker push wesparish/curator:5.4.1-1.1
